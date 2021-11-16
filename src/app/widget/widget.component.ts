7668d12526fe1d74c14c0cfa5aaecee7747980a8
kinpets <yeah@sure.com>
2021-11-16 13:14:20 +0100

    add error handling and server day info

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -12,7 +12,8 @@ interface Player {
 
 interface ServerResponse {
   info: { Map: string, MaxPlayers: number, Players: number };
-  players: Player[]
+  players: Player[],
+  rules: { DayTime_s: string }
 }
 
 @Component({
@@ -28,6 +29,8 @@ export class WidgetComponent implements OnInit, OnDestroy {
 
   players: any;
   serverInfo: any;
+  serverDay: string = '0';
+  somethingWentWrong = false;
   displayedColumns: string[] = ['Name', 'TimeF'];
   constructor(private http: HttpClient) { }
 
@@ -46,9 +49,15 @@ export class WidgetComponent implements OnInit, OnDestroy {
 
   loadPlayers() {
     this.http.get<ServerResponse>(`${this.queryUrl}${this.ipport}`).subscribe(res => {
-      this.players = res.players;
-      this.serverInfo = res.info;
-    });
+      if (res && res.info) {
+        this.players = res.players;
+        this.serverInfo = res.info;
+        this.serverDay = res.rules.DayTime_s;
+        this.somethingWentWrong = false;
+      } else {
+        this.somethingWentWrong = true;
+      }
+    }, () => this.somethingWentWrong = true); // on error but it gives 200 anyway
   }
 
 }
